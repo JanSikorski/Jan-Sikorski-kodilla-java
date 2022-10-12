efd8c4c903f75ce9fd6e0bedf33992fa50118701
Jan Sikorski <jans3876@gmail.com>
2022-10-12 23:26:54 +0200

    6.2 First test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.claculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("6. Wprowadzenie do testowania oprogramowania");
+
+        String input = "the user";
+        SimpleUser simpleUser = new SimpleUser(input);
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals(input)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("error!");
+        }
+
+        //test - calculator
+
+        if (Calculator.add(1, 2) == 3 && Calculator.subtract(2, 2) == 0) {
+            System.out.println("Calculations OK!!!");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
