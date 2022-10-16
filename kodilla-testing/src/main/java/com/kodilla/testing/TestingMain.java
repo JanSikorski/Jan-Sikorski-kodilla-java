6881238def35970b528a9d08220abdd4f299f2e3
Jan Sikorski <jans3876@gmail.com>
2022-10-16 22:04:55 +0200

    Added information which class is print about

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -12,19 +12,19 @@ public class TestingMain {
         String result = simpleUser.getUserName();
 
         if (result.equals(input)) {
-            System.out.println("test OK");
+            System.out.println("test OK (SimpleUser class)");
         } else {
-            System.out.println("error!");
+            System.out.println("error (SimpleUser class)!");
         }
 
         //test - calculator
 
         if (Calculator.add(1, 2) == 3) {
-            System.out.println("Adding OK!!!");
+            System.out.println("Adding OK (Calculator class)");
         } else if (Calculator.subtract(2, 2) == 0) {
-            System.out.println("Subtraction OK!!!");
+            System.out.println("Subtraction OK (Calculator class");
         } else {
-            System.out.println("Error! Both wrong");
+            System.out.println("Error! Both wrong (Calculator class");
         }
     }
 }
