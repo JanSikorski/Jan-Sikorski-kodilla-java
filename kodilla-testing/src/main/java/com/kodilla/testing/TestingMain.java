7e9a313c5f51236f945fb5209c1a1a63e069b15b
Jan Sikorski <jans3876@gmail.com>
2022-10-24 22:56:04 +0200

    added average and median methods for WeatherForecast class. Added tests for these methods in WeatherForecastTestSuite class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,13 +1,10 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.claculator.Calculator;
-import com.kodilla.testing.shape.ShapeCollector;
-import com.kodilla.testing.shape.Square;
-import com.kodilla.testing.shape.Triangle;
-import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
+
     public static void main(String[] args) {
 
     }
+
+
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,7 +1,6 @@
 package com.kodilla.testing.weather.stub;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class WeatherForecast {
     private Temperatures temperatures;
@@ -21,5 +20,38 @@ public class WeatherForecast {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
         }
         return resultMap;
+    }
+
+    public double averageTemperature() {
+        Double sum = 0.0;
+
+        for (Map.Entry<String, Double> temperature: temperatures.getTemperatures().entrySet()) {
+            sum += temperature.getValue();
+        }
+
+        return sum / temperatures.getTemperatures().size();
+    }
+
+    private List<Double> sortTemperatures() {
+
+        List<Double> sortedTemperatures = new ArrayList<>();
+        for (Map.Entry<String, Double> temperature: temperatures.getTemperatures().entrySet()) {
+            sortedTemperatures.add(temperature.getValue());
+        }
+
+        Collections.sort(sortedTemperatures);
+
+        return sortedTemperatures;
+    }
+    public double medianTemperature() {
+
+        if (sortTemperatures().size() % 2 == 0) {
+            double lowerValue = sortTemperatures().get(sortTemperatures().size() / 2);
+            double higherValue = sortTemperatures().get(sortTemperatures().size() / 2 - 1);
+
+            return lowerValue / higherValue;
+        } else {
+            return sortTemperatures().get(sortTemperatures().size() / 2);
+        }
     }
 }
