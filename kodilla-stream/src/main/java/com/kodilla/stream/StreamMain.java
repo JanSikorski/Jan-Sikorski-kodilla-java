9cf5f79ce3b753faf0cc83880b9bf1ae3057c356
Jan Sikorski <jans3876@gmail.com>
2022-11-01 13:27:06 +0100

    m

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,17 +2,22 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
-import com.kodilla.stream.immutable.NumbersGenerator;
+import com.kodilla.stream.forumUser.Forum;
+import com.kodilla.stream.forumUser.ForumUser;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.person.People;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamMain {
 
     public static void main(String[] args) {
+
         BookDirectory bookDirectory= new BookDirectory();
         String theResultStringOfBooks = bookDirectory.getList().stream()
                 .filter(book -> book.getYearOfPublication() > 2005)
@@ -20,5 +25,17 @@ public class StreamMain {
                 .collect(Collectors.joining(",\n", "<<", ">>"));
 
         System.out.println(theResultStringOfBooks);
+
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> forumUsers = forum.getUserList().stream()
+                .filter(user -> user.getUserGender() == 'M')
+                .filter(user -> LocalDate.now().getYear() - user.getUserBirthDate().getYear() > 20)
+                .filter(user -> user.getUserNumberOfPosts() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserId, user -> user));
+
+        System.out.println(forumUsers.size() + " users found");
+        forumUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + "\t:\t" + entry.getValue())
+                .forEach(System.out::println);
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/forumUser/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumUser/ForumUser.java
@@ -1,2 +1,50 @@
-package com.kodilla.stream.forumUser;public class ForumUser {
+package com.kodilla.stream.forumUser;
+
+import java.time.LocalDate;
+
+public final class ForumUser {
+    private final int userId;
+    private final String userName;
+    private final char userGender;
+    private final LocalDate userBirthDate;
+    private final int userNumberOfPosts;
+
+    public ForumUser(int userId, String userName, char userGender, LocalDate userBirthDate, int userNumberOfPosts) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userGender = userGender;
+        this.userBirthDate = userBirthDate;
+        this.userNumberOfPosts = userNumberOfPosts;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public char getUserGender() {
+        return userGender;
+    }
+
+    public LocalDate getUserBirthDate() {
+        return userBirthDate;
+    }
+
+    public int getUserNumberOfPosts() {
+        return userNumberOfPosts;
+    }
+
+    @Override
+    public String toString() {
+        return "ForumUser{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userGender=" + userGender +
+                ", userBirthDate=" + userBirthDate +
+                ", userNumberOfPosts=" + userNumberOfPosts +
+                '}';
+    }
 }
