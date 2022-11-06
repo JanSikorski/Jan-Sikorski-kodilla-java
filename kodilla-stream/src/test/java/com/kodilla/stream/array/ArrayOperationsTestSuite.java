c8672f0871d3b68ad2b62b9fe4848de90be7b54e
Jan Sikorski <jans3876@gmail.com>
2022-11-07 00:06:34 +0100

    JDX-7.5

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -12,11 +12,11 @@ public class ArrayOperationsTestSuite {
     @Test
     void testGetAverage() {
         //Given
-        int[] numbers = new int[]{1, 2, 5, 12, 40, 0};
+        int[] numbers = new int[]{1, 2, 5, 12, 43, 0, -1};
         //When
         double average = ArrayOperations.getAverage(numbers);
 
         //Then
-        assertEquals(10, average);
+        assertEquals(8.857, average, 0.0002);
     }
 }
