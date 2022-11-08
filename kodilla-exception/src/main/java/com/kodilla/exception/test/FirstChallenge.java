5752c85eac4890c1f3c6c660fc0e7851ca5395f8
Jan Sikorski <jans3876@gmail.com>
2022-11-08 22:23:31 +0100

    moved try/catch to main method

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,19 +2,12 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
 
-            return a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("Your exception: " + e);
-            return 0;
-        } finally {
-            System.out.println("End of calculations");
+        if (b == 0) {
+            throw new ArithmeticException();
         }
 
+        return a / b;
     }
 
     /**
@@ -24,7 +17,14 @@ public class FirstChallenge {
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 0);
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Your Exception: " + e);
+        } finally {
+            System.out.println("End task");
+        }
+
     }
 }
