f119401b50b2254747662b77d27d6c3d78c2563a
Jan Sikorski <jans3876@gmail.com>
2022-11-09 17:34:48 +0100

    m

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightSearch {
+
+    public Flight findFlight(Flight flight) {
+        Map<String, Boolean> aviableAirports = new HashMap<>();
+
+         flight.getArrivalAirport();
+
+         return null;
+    }
+}
